4b5a29b3ae2725f983c9f3c2305bdcacd3aa8d44
cjunks94 <cjunks94@gmail.com>
2018-05-02 14:14:10 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? '.'
   end
 
   def question?
-
+    self.end_with? '?'
   end
 
   def exclamation?
-
+    self.end_with? '!'
   end
 
   def count_sentences
-
+#split. arrylength. win.
+  arr=[]
+  arr = self.split(/\.|\?|\!/)
+  arr.delete_if {|val| val == ''}
+  arr.length
   end
 end
